16aa2e7cd099312e69eedad810f10c8bdaf10c55
nacer <nacer@DESKTOP-DBGQ78E>
2022-05-04 18:12:03 +0000

    projet QuickBank, first commit.

---
+++ BackEndQuickBank/src/main/java/backEndQuickBank/BackEndQuickBankApplication.java
@@ -0,0 +1,13 @@
+package backEndQuickBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndQuickBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndQuickBankApplication.class, args);
+	}
+
+}
---
+++ BackEndQuickBank/src/main/java/backEndQuickBank/exchangeAPI/controller/ExchangeEndPoint.java
@@ -0,0 +1,50 @@
+package backEndQuickBank.exchangeAPI.controller;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping(path = "/convertion")
+public class ExchangeEndPoint {
+	
+	@GetMapping
+	public JsonNode getConnection(
+			@RequestParam(name ="us") String unit,
+			@RequestParam("ud") String unitDest) throws Exception 
+	{
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("https://exchangerate-api.p.rapidapi.com/rapid/latest/"+unit))
+				.header("X-RapidAPI-Host", "exchangerate-api.p.rapidapi.com")
+				.header("X-RapidAPI-Key", "e3f3bb4695msh8c8e6a4266e4929p1caa86jsn04ff1793757e")
+				.method("GET", HttpRequest.BodyPublishers.noBody())
+				.build();
+		HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+		//System.out.println(response.body());
+		String resApiConnection = response.body();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		JsonNode jsonresultat =mapper.readTree(resApiConnection);
+		
+		jsonresultat=jsonresultat.get("rates");
+		
+		JsonNode jsonresultat2 = jsonresultat.get(unitDest.toUpperCase());
+		
+		double t=jsonresultat2.asDouble();
+		System.out.println("le taux est "+t);
+		
+		return jsonresultat2;
+	}
+	
+}
